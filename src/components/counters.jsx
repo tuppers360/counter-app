2142338fa24a51e1fa5196317a324406cd6be520
tuppers360 <tuppers360@live.co.uk>
2019-05-24 21:05:38 +0100

    10 - Removing the Local State

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,8 +11,15 @@ class Counters extends Component {
     ]
   };
 
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
   handleDelete = counterId => {
-    console.log("Event Handler Called", counterId);
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters });
   };
@@ -38,6 +45,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
